3d35759fcbfee57c1a7482f22f43d8afb95894ca
Jun <wnsghd1@naver.com>
2023-04-01 23:24:32 +0900

    merge sort

---
+++ sort/mergeSort.js
@@ -0,0 +1,11 @@
+const mergeSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const middle = Math.floor(arr.length / 2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+export default mergeSort;
