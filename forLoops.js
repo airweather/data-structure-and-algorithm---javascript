9dbdbe3c8cb5442b220ae4d2ec7abdbeaf4845ed
Jun <wnsghd1@naver.com>
2023-04-10 23:35:08 +0900

    reduce

--- forLoops.js
+++ forLoops.js
@@ -3,9 +3,17 @@ const arr = ['apple', 'graph', 'banana', 'orange', 'mango', 'pineapple', 'strawb
 //for in 문에는 배열의 인덱스가 출력된다.
 for(const item in arr) {
     console.log(item);
-}
+};
 
 //for of 문에는 배열의 요소가 출력된다.
 for(const item of arr) {
     console.log(item);
-}
+};
+
+const numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//acc는 누적값, cur은 현재값
+numberArr.reduce((acc, cur) => {
+    console.log(acc, cur);
+    return acc + cur;
+}, 0);
