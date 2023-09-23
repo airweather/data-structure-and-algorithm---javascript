93c9662e15bba29afb505aea170d76b21d0004ca
Jun <wnsghd1@naver.com>
2023-09-24 00:28:30 +0900

    Reducer Initial Value

--- reduce.js
+++ reduce.js
@@ -16,14 +16,3 @@ array1.reduce((acc, cur, idx, src) => {
 // 콜백의 최초 호출 때 accumulator와 currentValue는 다음 두 가지 값 중 하나를 가질 수 있습니다. 
 // 만약 reduce() 함수 호출에서 initialValue를 제공한 경우, accumulator는 initialValue와 같고 currentValue는 배열의 첫 번째 값과 같습니다. 
 // initialValue를 제공하지 않았다면, accumulator는 배열의 첫 번째 값과 같고 currentValue는 두 번째와 같습니다.
-
-var maxCallback = (acc, cur) => Math.max(acc.x, cur.x);
-var maxCallback2 = (max, cur) => Math.max(max, cur);
-
-// initialValue 없이 reduce()
-[{ x: 22 }, { x: 42 }].reduce(maxCallback); // 42
-[{ x: 22 }].reduce(maxCallback); // { x: 22 }
-[].reduce(maxCallback); // TypeError
-
-// map/reduce로 개선 - 비었거나 더 큰 배열에서도 동작함
-[{ x: 22 }, { x: 42 }].map((el) => el.x).reduce(maxCallback2, -Infinity);
